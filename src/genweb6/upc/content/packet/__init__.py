eba5bed94b45e0ea427346be03eea02469f29329
Iago López <ilopezsmx2011@gmail.com>
2022-10-24 11:33:08 +0200

    Contenido UPC

--- src/genweb6/upc/content/packet/__init__.py
+++ src/genweb6/upc/content/packet/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
+from genweb6.upc import _
 
 PACKETS_KEY = 'genweb.packets'
+
+I18N_KEYS = [_("fitxa_grau"), _("pla_grau"), _("fitxa_master"),
+             _("pla_estudis_master"), _("grups_recerca"),
+             _("grups_recerca_people"), _("contingut_genweb")]
