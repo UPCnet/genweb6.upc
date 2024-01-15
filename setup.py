80bd3fecc76b7e7a93bf16229d7af2adb4c9c9c6
Pilar Marinas <pilar.marinas@upcnet.es>
2024-01-15 11:56:01 +0100

    Back to development: 1.13

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.upc',
-    version='1.12',
+    version='1.13.dev0',
     description="Genweb 6 UPC package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
