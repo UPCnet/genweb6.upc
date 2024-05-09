9ccfb806ad475698a9be3bc588303f00d16ecbc4
ruben.padilla.mateu <ruben.padilla.mateu@ithinkupc.com>
2024-05-09 14:47:11 +0200

    Added event creator class

---
+++ src/genweb6/upc/browser/utils/event_creator.py
@@ -0,0 +1,43 @@
+import requests
+import os
+from zExceptions import BadRequest
+
+
+class EventCreator():
+
+    def __init__(self, event_data):
+        self.event_data = event_data
+        self.token = None
+        self.is_authenticated = False
+        self.login_name = os.environ.get('home_user', False)
+        self.password = os.environ.get('home_pass', False)
+        self.BASE_URL = 'https://webupcpre.upc.edu/'
+        self.headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+    def authenticate(self):
+        if not self.login_name or not self.password:
+            raise BadRequest("No s'han trobat les credencials per fer la petició")
+        
+        response = requests.post(f'{self.BASE_URL}@login', headers=self.headers, 
+                                json={'login': self.login_name, 'password': self.password})
+        
+        if not response.ok:
+            raise BadRequest("Hi ha hagut un error en el procés d'autenticació")
+        
+        self.token = response.json().get('token')
+        self.is_authenticated = True
+        self.headers['Authorization'] = f'Bearer {self.token}'
+
+    def create_event(self):
+        if not self.is_authenticated:
+            raise BadRequest("No es pot fer la petició. Ha fallat l'autenticació.")
+        
+        response = requests.post(f'{self.BASE_URL}/ca/agenda/esdeveniments', headers=self.headers, json=self.event_data)
+        if not response.ok:
+            raise BadRequest("Hi ha hagut un error creant l'esdeveniment")
+        
+        return response.json()
+        
+    
+
+    
