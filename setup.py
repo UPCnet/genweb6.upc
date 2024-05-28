06a454b037fdba6cc89671fa1acc4219a92e326d
ruben.padilla.mateu <ruben.padilla.mateu@ithinkupc.com>
2024-05-28 12:02:49 +0200

    Back to development: 1.21

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.upc',
-    version='1.20',
+    version='1.21.dev0',
     description="Genweb 6 UPC package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
