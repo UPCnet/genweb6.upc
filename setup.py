f9275b57a541c6dd90e61ebe56a0744d535fb178
Iago López <iago.lopez@upcnet.es>
2026-04-07 09:49:32 +0200

    Back to development: 1.38

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.upc',
-    version='1.37',
+    version='1.38.dev0',
     description="Genweb 6 UPC package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
